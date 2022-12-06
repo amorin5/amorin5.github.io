05860ceebf6fff6907462ff6d9366b83967a3f89
eertl2 <eertl2@wisc.edu>
2022-12-05 23:27:29 -0600

    almost working

---
+++ assets/js/firebase.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
+import { collection, getDocs, addDoc, Timestamp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
+import { query, orderBy, limit, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyChcoDH9YP09TZcx5fatDdBWcFzi7A476s",
+    authDomain: "pothole-4a96f.firebaseapp.com",
+    projectId: "pothole-4a96f",
+    storageBucket: "pothole-4a96f.appspot.com",
+    messagingSenderId: "829275371861",
+    appId: "1:829275371861:web:d6485f64d2be88a24317dc",
+    measurementId: "G-MF1GHZFNQ9"
+};
+    
+export async function getData() {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  const querySnapshot = await getDocs(collection(db, "markers"));
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+}
+
+// export { app, db, collection, getDocs, Timestamp, addDoc };
+// export { query, orderBy, limit, where, onSnapshot };
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,16 @@
+import { getData } from "./firebase.js"
+
 let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 43.073051, lng: -89.401230 },
     zoom: 8,
   });
+
+  let d2 = getData();
+
+  
 }
 
 window.initMap = initMap;
